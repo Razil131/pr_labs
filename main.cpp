b9a3cdda2d68b8795b73b3c195fd419de74addd7
inr7 <inr7@tpu.ru>
2025-09-15 13:22:03 +0700

    system('pause')

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 
 #include <iostream>
 
-int main (int, char **) {
+int main(int, char **)
+{
     std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
+    system("pause");
     return 0;
 }
